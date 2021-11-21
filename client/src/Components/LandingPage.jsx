beb67c132aeff8e189bd427b65841a484c515383
SaravanakumarJN <kumarjnsaravana@gmail.com>
2021-11-21 13:29:26 +0530

    updates

--- client/src/Components/LandingPage.jsx
+++ client/src/Components/LandingPage.jsx
@@ -14,66 +14,45 @@ import { listItemIconClasses } from "@mui/material";
 // } from "react-places-autocomplete";
 
 export const LandingPage = ({ setLogin }) => {
+  const details = JSON.parse(localStorage.getItem("GoogleDetails"));
+  const [location, setLocation] = useState([]);
+  function getLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.watchPosition(showPosition);
+    } else {
+      setLocation("Geolocation is not supported by this browser.");
+    }
+  }
 
-	const [location, setLocation] = useState([]);
-	// const [address, setAddress] = useState("");
-	// const [coordinates, setCoordinates] = useState({
-	// 	lat: null,
-	// 	lng: null,
-	// });
-	function getLocation() {
-		if (navigator.geolocation) {
-			navigator.geolocation.watchPosition(showPosition);
-		} else {
-			setLocation("Geolocation is not supported by this browser.");
-		}
-	}
+  function showPosition(position) {
+    console.log("position:", position);
+    setLocation(() => [position.coords.latitude, position.coords.longitude]);
+    localStorage.setItem(
+      "startpoints",
+      JSON.stringify({
+        lat: position.coords.latitude,
+        long: position.coords.longitude,
+      })
+    );
+  }
 
-	function showPosition(position) {
-		console.log("position:", position);
-		setLocation(() => [
-			position.coords.latitude,
-			position.coords.longitude,
-		]);
-		localStorage.setItem(
-			"startpoints",
-			JSON.stringify({
-				lat: position.coords.latitude,
-				long: position.coords.longitude,
-			})
-		);
-	}
-	// const handleSelect = async (value) => {
-	// 	const results = await geocodeByAddress(value);
-	// 	console.log("results:", results);
-	// 	const latLng = await getLatLng(results[0]);
-	// 	setAddress(value);
-	// 	setCoordinates(latLng);
-	// 	setLocation([latLng.lat, latLng.lng]);
-	// };
+  useEffect(() => {
+    getLocation();
+  }, []);
 
-
-	useEffect(() => {
-		getLocation();
-	}, []);
-	// useEffect(() => {
-	// 	window.location.reload();
-	// },[location])
-
-
-	return (
-		<>
-			<div className="d-flex m-2 justify-content-between">
-				<div className="d-flex">
-					<MenuIcon />
-					<p className="ms-3">Welcome, Sabareesh</p>
-				</div>
-				<Link to="/">
-					<LogoutIcon />
-				</Link>
-			</div>
-			{/* <Inputbox /> */}
-			{/* <PlacesAutocomplete
+  return (
+    <>
+      <div className='d-flex m-2 justify-content-between'>
+        <div className='d-flex'>
+          <MenuIcon />
+          <p className='ms-3'>Welcome, {details.givenName}</p>
+        </div>
+        <Link to='/'>
+          <LogoutIcon />
+        </Link>
+      </div>
+      {/* <Inputbox /> */}
+      {/* <PlacesAutocomplete
 
 				value={address}
 				onChange={setAddress}
@@ -117,31 +96,29 @@ export const LandingPage = ({ setLogin }) => {
 				)}
 			</PlacesAutocomplete> */}
 
-
-			<p className="lead mt-5">
-				Type your destination point and find out all parking spots
-				available for booking at the moment
-			</p>
-			<div>
-				<input
-					type="text"
-					value={location}
-					onChange={(e) => {
-						setLocation(e.target.value);
-					}}
-					className="form-control mt-5 text-center"
-					placeholder="Enter your destination"
-				/>
-			</div>
-			<Link to="/spots">
-				<button className="btn btn-outline-secondary mt-5 mb-5">
-					Search Now
-				</button>
-			</Link>
-			<div style={{ height: "170px", width: "360px" }}>
-				<MapContainer location={location} />
-			</div>
-		</>
-	);
-
+      <p className='lead mt-5'>
+        Type your destination point and find out all parking spots available for
+        booking at the moment
+      </p>
+      <div>
+        <input
+          type='text'
+          value={location}
+          onChange={(e) => {
+            setLocation(e.target.value);
+          }}
+          className='form-control mt-5 text-center'
+          placeholder='Enter your destination'
+        />
+      </div>
+      <Link to='/spots'>
+        <button className='btn btn-outline-secondary mt-5 mb-5'>
+          Search Now
+        </button>
+      </Link>
+      <div style={{ height: "170px", width: "360px" }}>
+        <MapContainer location={location} />
+      </div>
+    </>
+  );
 };
